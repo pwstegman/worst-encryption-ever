0418573dd0534ffb0dbc91fd8af440d719cc50e2
Pierce Stegman <pwstegman@gmail.com>
2015-10-31 23:35:17 -0400

    Made functions

--- encrypt.py
+++ encrypt.py
@@ -1,6 +1,13 @@
-message = input("What is your message? ")
-key = input("What is your key? ")
-klen = len(key)
-encrypted = "".join([message[i] + key[i%klen] for i in range(len(message))])
-print("Encrypted message")
-print(encrypted)
+def prompt()
+	message = input("What is your message? ")
+	key = input("What is your key? ")
+  encrypted = encrypt(message, key)
+	print("Encrypted message")
+	print(encrypted)
+
+def encrypt(message, key):
+  klen = len(key)
+  return "".join([message[i] + key[i%klen] for i in range(len(message))])
+
+if __name__ == "__main__":
+  prompt()
